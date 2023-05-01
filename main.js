3ff4a5b579a89f1800e6e09708b9cc0419c77044
Bruno Guimarães <bruno.guima00@gmail.com>
2023-04-30 21:39:15 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('form-calculo')
+
+form.addEventListener('submit',function(e){
+    e.preventDefault(); /* O evento que vai ser disparado */
+
+    const campoA = document.getElementById('number-a'); 
+    const campoB = document.getElementById('number-b');
+
+    if(campoA.value < campoB.value) {
+        alert('Formulário Válido');
+    } else {
+        alert('Formulário Inválido');
+    }    
+})
